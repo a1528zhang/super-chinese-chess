713438936c1095be615f7dd6777301157a86d00b
az <beingszhang@gmail.com>
2021-08-20 18:55:30 +0800

    refactor: refactoring

---
+++ src/state/MessageManager.ts
@@ -0,0 +1,62 @@
+import type { ComponentType } from "../component/checkerBoard/CheckerBoard.js";
+
+export enum MessageType {
+    Reset = "Reset", // 棋盘重置
+    Exchange = "Exchange",   // 玩家交换回合
+    Operating = "Operating",  // 玩家操作
+    Move = "Move",   // 棋子移动
+    Attack = "Attack", // 棋子攻击
+    //TODO
+}
+
+export type Listener = {
+    [messageType in MessageType]: MessageCallback[];
+};
+export type MessageCallback = () => void;
+/**
+ * 消息管理器
+ * 全局所有对象的操作都发送消息，
+ * 消息接受后调用 BoardManager
+ */
+class MessageManager {
+
+    private listeners: Listener = {} as Listener;
+
+    public addEventListener(messageType: MessageType, callback: MessageCallback): void {
+        if(!(messageType in this.listeners)) {
+            this.listeners[messageType] = [];
+        }
+        this.listeners[messageType].push(callback);
+    }
+
+    public removeEventListener(messageType: MessageType, callback?: MessageCallback): void {
+        if(!(messageType in this.listeners)) {
+            return;
+        } else {
+            const stack = this.listeners[messageType];
+            if (!callback) {
+                stack.length = 0;
+            } else {
+                for(let i = 0, l = stack.length; i < l; i ++) {
+                    if(stack[i] === callback){
+                        stack.splice(i, 1);
+                        return this.removeEventListener(messageType, callback);
+                    }
+                }
+            }
+        }
+    }
+
+    public dispatchMessage(messageType: MessageType): void {
+        if(!(messageType in this.listeners)) {
+            return;
+        }
+        const stack = this.listeners[messageType];
+        for(let i = 0, l = stack.length; i < l; i ++) {
+            stack[i].call(this, event);
+        }
+    }
+
+}
+
+export default new MessageManager();
---
+++ src/state/StateManager.ts
@@ -0,0 +1,28 @@
+import type BoardManager from "./BoardManager.js";
+
+export type State = {
+
+}
+/**
+ * 状态管理
+ * 所有操作调用 stateManager 修改状态
+ * 状态修改成功后收到状态回调，然后调用 boardManager 修改渲染结果
+ * 状态分为本地状态和远程状态，本地状态缓存远程状态，作为判断状态变化的依据。用于触发动画
+ */
+export class StateManager {
+
+    private localState: State;
+    
+    constructor(boardManager: BoardManager) {}
+
+    public setState(): void {
+        
+    }
+    /**
+     * 收到回调后调用 boardManager
+     */
+    public onStateChage(state: State): void {
+
+    }
+
+}
