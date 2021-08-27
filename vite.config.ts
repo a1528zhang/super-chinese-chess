0d62eecac0aedc7a8d79274e5414b5cb53779684
az <beingszhang@gmail.com>
2021-08-27 20:30:32 +0800

    feat: use vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   // 配置选项
   publicDir: "public",
   server: {
     port: 3000,
     strictPort: true,
   }
-
-}
+});
